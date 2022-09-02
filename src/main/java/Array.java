0fbff90499e4b79eabb6d119138468bf053a4057
sameer.kalekar <isamir.30@gmail.com>
2022-09-02 17:41:44 +0100

    commited two files

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,10 +1,8 @@
 package src.main.java;
 
-public class helloWorld {
+public class Array {
     public static void main(String args[]){
-        System.out.println("Hello World!!");
-
-        System.out.println(" updated feature code1.1");
+        System.out.println("other file");
 
         System.out.println("git reset HEAD <FILENAME>");
     }
